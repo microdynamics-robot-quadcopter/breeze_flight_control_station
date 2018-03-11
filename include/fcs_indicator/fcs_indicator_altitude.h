a16390f78292214af8c2f7587bbdc8d7153f615e
myyerrol <myyerrol@126.com>
2018-03-11 09:28:11 +0800

    Modify notice from BSD to GPL v3

--- include/fcs_indicator/fcs_indicator_altitude.h
+++ include/fcs_indicator/fcs_indicator_altitude.h
@@ -1,46 +1,43 @@
-/***********************************************************************
- *  Software License Agreement (BSD License)
+/******************************************************************************
+ * Software License Agreement (GPL V3 License)
  *
- *  Copyright (c) 2016, Team MicroDynamics
- *  All rights reserved.
+ * Copyright (C) 2014 bushuhui
+ * Copyright (c) 2016 myyerrol(Team MicroDynamics)
  *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
+ * This file is part of breeze_flight_control_station.
  *
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *  * Neither the name of the Team MicroDynamics nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- ***********************************************************************
+ * breeze_flight_control_station is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
 
- ***********************************************************************
- *  History:
- *  <Authors>        <Date>        <Operation>
- *  myyerrol         2016.7.12     Finish the basic gui
+ * breeze_flight_control_station is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with breeze_flight_control_station.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************
+
+ ******************************************************************************
+ * File:
+ * fcs_indicator_altitude.h
  *
- *  Description:
- *  This .h file defines some elements for implementing the gui of
- *  flight altitude indicator.
- **********************************************************************/
+ * Author:
+ * bushuhui<bushuhui@gmail.com>
+ * myyerrol<myyerrol@126.com>
+ *
+ * History:
+ * <Authors>     <Date>         <Version>     <Operation>
+ * bushuhui      2014.12.03     --           Finish altitude indicator
+ * myyerrol      2016.07.12     1.0          Modify altitude indicator
+ *
+ * Description:
+ * This .h file defines some elements for implementing the indicator of
+ * altitude according to qFlightInstruments project.
+ *****************************************************************************/
 
 #ifndef FLIGHT_ALTITUDE_INDICATOR_H
 #define FLIGHT_ALTITUDE_INDICATOR_H
