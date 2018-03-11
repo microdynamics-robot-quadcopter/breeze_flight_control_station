4e71a160ac8bcc67715f9db3fd5be602215d4192
myyerrol <myyerrol@126.com>
2018-03-11 09:00:44 +0800

    Add GPL v3 notice

--- include/fcs_instrucment/fcs_instrucment_alt.h
+++ include/fcs_instrucment/fcs_instrucment_alt.h
@@ -1,3 +1,44 @@
+/******************************************************************************
+ * Software License Agreement (GPL V3 License)
+ *
+ * Copyright (C) 2013 Marek M. Cel
+ * Copyright (c) 2018 myyerrol(Team MicroDynamics)
+ *
+ * This file is part of breeze_flight_control_station.
+ *
+ * breeze_flight_control_station is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+
+ * breeze_flight_control_station is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with breeze_flight_control_station.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************
+
+ ******************************************************************************
+ * File:
+ * fcs_instrucment_alt.h
+ *
+ * Author:
+ * Marek M. Cel<marekcel@marekcel.pl>
+ * myyerrol<myyerrol@126.com>
+ *
+ * History:
+ * <Authors>     <Date>         <Version>     <Operation>
+ * Marek M. Cel  2013.--.--     --            Finish ALT instrument
+ * myyerrol      2018.03.xx     1.0           Modify ALT instrument
+ *
+ * Description:
+ * This .h file defines some elements for implementing the instrument of
+ * Altimeter(ALT) according to QFlightInstruments project.
+ *****************************************************************************/
+
 #ifndef FCS_INSTRUCMENT_ALTITUDE_H
 #define FCS_INSTRUCMENT_ALTITUDE_H
 
